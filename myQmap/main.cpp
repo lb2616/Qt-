dd9cc1730a6fe8d6489e918e9f70238491cce4de
libo <libo@pset.suntec.net>
2016-07-02 21:00:39 +0800

    Qtimer

--- myQmap/main.cpp
+++ myQmap/main.cpp
@@ -4,12 +4,12 @@
 int main(int argc, char *argv[])
 {
     //QCoreApplication a(argc, argv);
-    QMap<QString,QString> map;
-    //
+    QMap<QString,QString> map;                       //创建一个QMap栈对象
+    //向栈对象插入<城市,区号>对
     map.insert("beijing","111");
     map.insert("shanghai","021");
     map.insert("nanjing","025");
-    QMapIterator<QString,QString> i(map);            //
+    QMapIterator<QString,QString> i(map);            //创建一个只读迭代器
     for (;i.hasNext();)
         qDebug()<<"  "<<i.key()<<"  "<<i.next().value();
     QMutableMapIterator<QString,QString> mi(map);
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
         mi.setValue("010");
     QMapIterator<QString,QString> modi(map);
     qDebug()<<" ";
-    for (;modi.hasNext();)
+    for (;modi.hasNext();)                           //再次遍历并输出修改后的结果
         qDebug()<<" "<<modi.key()<<"  "<<modi.next().value();
     return 0;
     //return a.exec();
