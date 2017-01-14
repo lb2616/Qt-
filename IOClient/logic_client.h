7b6978e852dc415974f274e375e8532d3e119ce9
libo <libo@pset.suntec.net>
2017-01-14 20:43:57 +0800

    IOClient and server

---
+++ IOClient/logic_client.h
@@ -0,0 +1,12 @@
+#ifndef LOGIC_CLIENT_H
+#define LOGIC_CLIENT_H
+
+#include "data_client.h"
+
+// 验证注册输入的用户名是否合法
+Status check_register_name(char username[]);
+
+// 验证注册两次输入的密码长度是否正确是否一致
+Status check_register_pwd( char userpwd[], char pwdagain[]);
+
+#endif // LOGIC_CLIENT_H_INCLUDED
---
+++ IOClient/ui_client.h
@@ -0,0 +1,12 @@
+#ifndef UI_CLIENT
+#define UI_CLIENT
+
+#include "logic_client.h"
+
+//2.客户端登录
+int ui_login(int sockfd);
+
+//3.登录注册总界面
+int ui_register_login(int sockfd);
+
+#endif // LOGIC_CLIENT_H
